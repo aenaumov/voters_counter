0fab9b877edc0508a241948afedd41593ab8d0e2
Alexander Naumov <a.e.naumov@yandex.ru>
2024-03-16 19:37:42 +0300

    Initial commit

---
+++ src/main/java/com/example/voters_counter/VotersCounterApplication.java
@@ -0,0 +1,13 @@
+package com.example.voters_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotersCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotersCounterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/voters_counter/VotersCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.voters_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotersCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
