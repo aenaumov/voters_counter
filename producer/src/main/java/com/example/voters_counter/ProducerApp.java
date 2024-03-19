722400f0fff1864c82404c92a8b741ee34d120ed
Alexander Naumov <a.e.naumov@yandex.ru>
2024-03-19 16:30:10 +0300

    create ProducerApp

---
+++ producer/src/main/java/com/example/voters_counter/ProducerApp.java
@@ -0,0 +1,12 @@
+package com.example.voters_counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApp {
+        public static void main(String[] args) {
+            SpringApplication.run(ProducerApp.class, args);
+        }
+
+    }
