fe26a98d97dc482829dc3e88f2d6b5f9f4f18f5e
Alexander Naumov <a.e.naumov@yandex.ru>
2024-03-26 12:54:16 +0300

    create UserReactiveRepository

---
+++ consumer/src/main/java/com/example/voters_counter/repository/UserReactiveRepository.java
@@ -0,0 +1,10 @@
+package com.example.voters_counter.repository;
+
+import com.example.voters_counter.entities.Person;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
+
+    public interface UserReactiveRepository extends R2dbcRepository<Person, Long> {
+        Mono<Person> findUserByFamily(String family);
+    }
+
