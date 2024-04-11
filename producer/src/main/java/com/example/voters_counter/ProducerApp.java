12f4925aa2d559ec37dd35b24538925f1ab0f41c
Alexander Naumov <a.e.naumov@yandex.ru>
2024-04-11 13:27:15 +0300

    refactor

--- producer/src/main/java/com/example/voters_counter/ProducerApp.java
+++ producer/src/main/java/com/example/voters_counter/ProducerApp.java
@@ -1,22 +1,16 @@
 package com.example.voters_counter;
 
-import com.example.voters_counter.dto.Votes;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.admin.NewTopic;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ProducerApp {
     public static void main(String[] args) {
         SpringApplication.run(ProducerApp.class, args);
     }
-    final ObjectMapper objectMapper = new ObjectMapper();
 
     @Bean
     public NewTopic topic() {
@@ -24,22 +18,6 @@ public class ProducerApp {
                 .partitions(1)
                 .replicas(1)
                 .build();
-    }
-
-    @Bean
-    public ApplicationRunner runner(KafkaTemplate<String, String> template) throws JsonProcessingException {
-
-        final Votes votes = Votes.builder()
-                .family("PUTIN")
-                .votes(1L)
-                .build();
-        final String jsonVotes = objectMapper.writeValueAsString(votes);
-
-        return args -> {
-            for (int i = 0; i < 21; i++) {
-                template.send("topic_vote", jsonVotes);
-            }
-        };
     }
 
 }
