c9e2471a7c08eebc8288ed079107b3298a9e8b4a
Alexander Naumov <a.e.naumov@yandex.ru>
2024-04-25 15:55:45 +0300

    create TopicConfig file

--- producer/src/main/java/com/example/voters_counter/ProducerApp.java
+++ producer/src/main/java/com/example/voters_counter/ProducerApp.java
@@ -1,23 +1,12 @@
 package com.example.voters_counter;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class ProducerApp {
     public static void main(String[] args) {
         SpringApplication.run(ProducerApp.class, args);
-    }
-
-    @Bean
-    public NewTopic topic() {
-        return TopicBuilder.name("topic_vote")
-                .partitions(1)
-                .replicas(1)
-                .build();
     }
 
 }
