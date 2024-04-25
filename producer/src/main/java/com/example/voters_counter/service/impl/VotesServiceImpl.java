6bd205fc69c7a3fdd4ce94fd41f23c128318eb80
Alexander Naumov <a.e.naumov@yandex.ru>
2024-04-25 15:56:54 +0300

    delete Transaction

--- producer/src/main/java/com/example/voters_counter/service/impl/VotesServiceImpl.java
+++ producer/src/main/java/com/example/voters_counter/service/impl/VotesServiceImpl.java
@@ -5,7 +5,6 @@ import com.example.voters_counter.service.VotesService;
 import lombok.AllArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
@@ -14,9 +13,8 @@ public class VotesServiceImpl implements VotesService {
     private final KafkaTemplate<String, Votes> template;
 
     @Override
-    @Transactional("kafkaTransactionManager")
     public void addVotes(Votes votes){
-         template.send("topic_vote", votes);
+         template.send("topic_vote", "vote", votes);
     }
 
 }
