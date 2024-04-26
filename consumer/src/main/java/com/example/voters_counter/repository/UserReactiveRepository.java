78df4c729f3cf658072d50382188b8a8bb879456
Alexander Naumov <a.e.naumov@yandex.ru>
2024-04-26 16:09:09 +0300

    refactor

--- consumer/src/main/java/com/example/voters_counter/repository/UserReactiveRepository.java
+++ consumer/src/main/java/com/example/voters_counter/repository/UserReactiveRepository.java
@@ -4,12 +4,11 @@ import com.example.voters_counter.entities.Person;
 import org.springframework.data.r2dbc.repository.R2dbcRepository;
 import org.springframework.data.relational.core.sql.LockMode;
 import org.springframework.data.relational.repository.Lock;
-import org.springframework.data.repository.query.Param;
 import reactor.core.publisher.Mono;
 
 public interface UserReactiveRepository extends R2dbcRepository<Person, Long> {
     @Lock(LockMode.PESSIMISTIC_WRITE)
-    Mono<Person> findPersonByFamily(@Param("family") String family);
+    Mono<Person> findPersonByFamily(String family);
 
 }
 
